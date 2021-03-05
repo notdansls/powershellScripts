695ba04044c34b4f5278e1a63f7aa55274944c47
notdansls <notdansls@gmail.com>
2021-03-05 17:03:08 +0000

    Creation of helloWorld.ps1 and initial comments

---
+++ helloWorld.ps1
@@ -0,0 +1,10 @@
+﻿# A simple hello world script
+
+# Save typing 'Hello world' over and over - just string it 
+$strHW = "Hello world";
+
+# Write-Host will write directly to the console
+Write-Host $strHW;
+
+# Write-Output will write to the pipeline so can be piped to the next command
+Write-Output $strHW;
