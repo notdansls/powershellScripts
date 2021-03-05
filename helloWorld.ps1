0c0b6332b85b52d6290226b2acd1903b18c2fc3a
notdansls <notdansls@gmail.com>
2021-03-05 17:07:56 +0000

    Added input to helloWorld.ps1

--- helloWorld.ps1
+++ helloWorld.ps1
@@ -4,7 +4,14 @@
 $strHW = "Hello world";
 
 # Write-Host will write directly to the console
-Write-Host $strHW;
+# Write-Host $strHW; # Commented out for now, want to add some input
 
 # Write-Output will write to the pipeline so can be piped to the next command
-Write-Output $strHW;
+# Write-Output $strHW; # Commented out for now, want to add some input
+
+# Ask for users name (not username)
+$strName = Read-Host -Prompt "Please enter your name"
+
+# Write welcome out to host
+Write-Host "Welcome $strName"
+Write-Host $strHW
